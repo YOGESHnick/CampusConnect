8c88149053a060162f985c852b2cced14f4a0c37
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-12-02 08:38:59 +0530

    push login

---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <form>
+        <input type="text" placeholder="Enter Roll.No" />
+        <input type="text" placeholder="Enter password" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
