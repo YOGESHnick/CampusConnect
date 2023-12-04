294f8616fc64c23f3c6ced743c717f7b99e60aa2
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-12-04 12:28:54 +0530

    lol

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,10 +3,15 @@ import React from "react";
 const Login = () => {
   return (
     <div className="login">
-      <form>
-        <input type="text" placeholder="Enter Roll.No" />
-        <input type="text" placeholder="Enter password" />
-      </form>
+      <div className="form">
+        <img src="clg.png" className="clg" alt="clg" />
+        <form>
+          <img src="logo.png" className="logo" alt="logo" />
+          <input type="text" placeholder="Enter Roll.No" />
+          <input type="text" placeholder="Enter password" />
+          <button>Login</button>
+        </form>
+      </div>
     </div>
   );
 };
