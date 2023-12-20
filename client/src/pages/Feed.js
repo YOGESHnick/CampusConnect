ccdf7614ef6662207da0478aa22042bb52b94df0
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-12-20 20:21:33 +0530

    moye moye

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useGetUserId } from "../hooks/useGetUserId";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import avatar from "../assets/profile.png";
 import Navbar from "../components/navbar.js";
 
 // L E F T   B A R
+
 const LeftBar = ({ userId, user }) => {
   return (
     <div className="LeftBar">
@@ -18,6 +20,37 @@ const LeftBar = ({ userId, user }) => {
 
 // F E E D   P O S T S  (at the centre)
 
+const NewPost = ({ userId, user }) => {
+  const navigate = useNavigate();
+  const [description, setDescription] = useState("");
+
+  const postHandler = async (event) => {
+    // event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8080/api/posts", {
+        userId:userId,
+        desc:description,
+      });
+      navigate("/feed");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="NewPost">
+      <img src={user?.dp || avatar} alt="img" />
+      <div>
+        <form onSubmit={postHandler}>
+          {/* <img src={user?.dp || avatar} alt="img" /> */}
+          <input type="text" placeholder="Share your thoughts" value={description} onChange={(event) => setDescription(event.target.value)} />
+          <button>Post</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
 const Posts = () => {
   return (
     <div className="Posts">
@@ -30,8 +63,8 @@ const QuickAccess = () => {
   return (
     <div className="QuickAccess">
       <h3>Quick Links</h3>
-      <a href="#" >Portal</a>
-      <a href="#" >Clg</a>
+      <a href="#">Portal</a>
+      <a href="#">Clg</a>
     </div>
   );
 };
@@ -66,6 +99,7 @@ const Feed = () => {
       <div className="Feed">
         <LeftBar userId={userId} user={user} />
         <div className="center">
+          <NewPost userId={userId} user={user} />
           <Posts />
         </div>
         <QuickAccess />
