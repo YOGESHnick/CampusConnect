b23825c85364d782be210d80eb4f6a9b9cbd8540
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2024-01-15 16:58:18 +0530

    gom

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ app.use(
 	})
   );
   
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(
 	cors({
 		origin: "http://localhost:3000",
